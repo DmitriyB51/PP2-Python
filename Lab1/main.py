baf03312552c1b233b69a0049db100d87e6688a7
DmitriyB51 <d.v.belinskiy51@mail.ru>
2024-01-21 18:54:29 +0600

    test

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,4 +1,61 @@
-print("sslmdlsm")
+# python HOME
+# example
+print("Hello, World!")
 
+# Python Syntax
+# ex1
+print("Hello World")
+# ex2
+if 5 > 2:
+    print("YES")
+
+# python Comments
+# example
+# This is a comment
+print("Hello, World!")
+# ex1
+# This is a comment
+# ex2
+"""
+This is a comment
+written in 
+more than just one line
+"""
+# python Variables
+# ex1
+carname = "Volvo"
+# ex2
+x = 50
+# ex3
 x = 5
-print(x)
+y = 10
+print(x + y)
+# ex4
+x = 5
+y = 10
+z = x + y
+print(z)
+# ex5
+x, y, z = "Orange", "Banana", "Cherry"
+# ex6
+x = y = z = "Orange"
+
+
+# ex7
+def myfunc():
+    global x
+    x = "fantastic"
+# Python data types
+# ex1
+x = 5
+print(type(x))
+int
+# ex2
+x = "Hello World"
+print(type(x))
+str
+# ex3
+x = 20.5
+print(type(x))
+float
+# ex4
